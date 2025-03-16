c3809163854dfbb54fb92762d334ad64e8e64346
surajgupt01 <surajgupt880@gmail.com>
2025-03-16 18:20:39 +0530

    up-6

--- src/App.tsx
+++ src/App.tsx
@@ -21,10 +21,10 @@ function handleInput(){
 
 
 
-<div className="w-full h-screen bg-gray-100 overflow-hidden">
+<div className="w-full h-full bg-gray-100  p-2">
 
 <div className="font-bold text-4xl p-3 text-gray-700 cursor-pointer flex">
-  <img src="https://www.reshot.com/preview-assets/icons/SU64G82BJK/data-cleaning-SU64G82BJK.svg" className="w-10 h-8"></img>
+  <img src="https://www.reshot.com/preview-assets/icons/SU64G82BJK/data-cleaning-SU64G82BJK.svg" className="w-10 h-10"></img>
   EDA-X
   </div>
 
